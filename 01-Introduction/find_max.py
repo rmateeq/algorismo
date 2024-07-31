c545f2b8fdca185118eee8db6e88bcfad6af1cb9
Muhammad Ateeq <rmateeq@yahoo.com>
2024-07-31 19:48:34 +0500

    Create find_max.py

---
+++ 01-Introduction/find_max.py
@@ -0,0 +1,6 @@
+sample_numbers = [5, 3, 9, 1, 6]
+max_number = numbers[0]
+for number in numbers:
+    if number > max_number:
+        max_number = number
+print("The maximum number is:", max_number)
