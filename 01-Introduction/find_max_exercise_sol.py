a6368fb17d4ac45a067c870f23f585fed84bdec2
Muhammad Ateeq <rmateeq@yahoo.com>
2024-07-31 22:28:35 +0500

    Update find_max_exercise_sol.py

--- 01-Introduction/find_max_exercise_sol.py
+++ 01-Introduction/find_max_exercise_sol.py
@@ -20,10 +20,22 @@ sample_numbers = [5, 3, 9, 1, 6]
 # Step count is: `5`
 # Variation 2: Larger list
 # sample_numbers = [8, 2, 10, 4, 7, 1, 12, 14, 3, 6]
-# Step count is: `21`
+# Step count is: `10`
 # Variation 3: Even larger list
 # sample_numbers = [15, 3, 9, 8, 22, 4, 13, 2, 11, 17, 1, 19, 6, 18, 7, 20, 5, 14, 10, 21, 16, 12]
-# Step count is: `21`
+# Step count is: `22`
 print("The maximum number is:", find_max(sample_numbers))
 
 # Genrally for a list of size `n`, the number of steps to find the maximum number is `n`.
+
+
+# Explanation of Steps:
+# For each list, the number of steps taken by the algorithm is equal to the number of elements in the list.
+# This is because the algorithm iterates through each element once to find the maximum number.
+# 
+# For example:
+# - For the list [5, 3, 9, 1, 6], there are 5 elements, so it takes 5 steps.
+# - For the list [8, 2, 10, 4, 7, 1, 12, 14, 3, 6], there are 10 elements, so it takes 10 steps.
+# - For the list [15, 3, 9, 8, 22, 4, 13, 2, 11, 17, 1, 19, 6, 18, 7, 20, 5, 14, 10, 21, 16, 12], there are 22 elements, so it takes 22 steps.
+#
+# In general, for a list of size n, the algorithm will take n steps because it needs to examine each element once to determine the maximum.
