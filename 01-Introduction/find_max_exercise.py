6a5ca09b6d3bb7abc58c60cade6714dca92d0af4
Muhammad Ateeq <rmateeq@yahoo.com>
2024-07-31 20:58:21 +0500

    Update find_max_exercise.py

--- 01-Introduction/find_max_exercise.py
+++ 01-Introduction/find_max_exercise.py
@@ -1,13 +1,32 @@
 def find_max(numbers):
-  # define and initialize a variable `steps` to 0
+  # Define and initialize a variable `steps` to 0
+  # 
   max_number = numbers[0]
   for number in numbers:
-    # increment steps here
+    # Increment steps here
+    # 
     if number > max_number:
       max_number = number
-  # print steps here
+  # Print steps here
+  # 
   return max_number
 
-# use the lists as input and take a count of `steps` (implemented inside the function `find_max`) it takes to find the maximum number from the input
-sample_numbers = [5, 3, 9, 1, 6]
-print("The maximum number is:", find_max(sample_numbers))
+# Use the following lists as input (one at a time) and take a count of `steps` (implemented inside the function `find_max`) it takes to find the maximum number from the input.
+# Try to observe the change in the count of `steps` with changing input size (i.e., length of the list) and generalize it.
+
+# Variation 1: Small list
+sample_numbers1 = [5, 3, 9, 1, 6]
+print("The maximum number is:", find_max(sample_numbers1))
+# Step count is: ____
+
+# Variation 2: Larger list
+sample_numbers2 = [8, 2, 10, 4, 7, 1, 12, 14, 3, 6]
+print("The maximum number is:", find_max(sample_numbers2))
+# Step count is: ____
+
+# Variation 3: Even larger list
+sample_numbers3 = [15, 3, 9, 8, 22, 4, 13, 2, 11, 17, 1, 19, 6, 18, 7, 20, 5, 14, 10, 21, 16, 12]
+print("The maximum number is:", find_max(sample_numbers3))
+# Step count is: ____
+
+# Genrally for a list of size `n`, the number of steps to find the maximum number is ____.
