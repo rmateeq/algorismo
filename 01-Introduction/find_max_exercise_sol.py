ec00df4d7f9d391fa7f5eac780c8af22c6bebc4f
Muhammad Ateeq <rmateeq@yahoo.com>
2024-07-31 22:20:14 +0500

    Update find_max_exercise_sol.py

--- 01-Introduction/find_max_exercise_sol.py
+++ 01-Introduction/find_max_exercise_sol.py
@@ -8,6 +8,7 @@ def find_max(numbers):
     if number > max_number:
       max_number = number
   # Print steps here
+  # The len() function returns the number of items in a list
   print("It took", steps, "steps to find the maximum number from a list of", len(numbers), "numbers")
   return max_number
 
@@ -15,13 +16,10 @@ def find_max(numbers):
 # Try to observe the change in the count of `steps` with changing input size (i.e., length of the list) and generalize it
 
 # Variation 1: Small list
-sample_numbers1 = [5, 3, 9, 1, 6]
-print("The maximum number is:", find_max(sample_numbers1))
-
+sample_numbers = [5, 3, 9, 1, 6]
 # Variation 2: Larger list
-sample_numbers2 = [8, 2, 10, 4, 7, 1, 12, 14, 3, 6]
-print("The maximum number is:", find_max(sample_numbers2))
-
+# sample_numbers = [8, 2, 10, 4, 7, 1, 12, 14, 3, 6]
 # Variation 3: Even larger list
-sample_numbers3 = [15, 3, 9, 8, 22, 4, 13, 2, 11, 17, 1, 19, 6, 18, 7, 20, 5, 14, 10, 21, 16, 12]
-print("The maximum number is:", find_max(sample_numbers3))
+# sample_numbers = [15, 3, 9, 8, 22, 4, 13, 2, 11, 17, 1, 19, 6, 18, 7, 20, 5, 14, 10, 21, 16, 12]
+
+print("The maximum number is:", find_max(sample_numbers))
