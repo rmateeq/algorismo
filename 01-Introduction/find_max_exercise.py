7c284ce1138c52375f854cc96dd7dd47bb843b98
Muhammad Ateeq <rmateeq@yahoo.com>
2024-07-31 20:01:01 +0500

    Update find_max_exercise.py

--- 01-Introduction/find_max_exercise.py
+++ 01-Introduction/find_max_exercise.py
@@ -1,1 +1,13 @@
+def find_max(numbers):
+  # define and initialize a variable `steps` to 0
+  max_number = numbers[0]
+  for number in numbers:
+    # increment steps here
+    if number > max_number:
+      max_number = number
+  # print steps here
+  return max_number
 
+# use the lists as input and take a count of `steps` (implemented inside the function `find_max`) it takes to find the maximum number from the input
+sample_numbers = [5, 3, 9, 1, 6]
+print("The maximum number is:", find_max(sample_numbers))
