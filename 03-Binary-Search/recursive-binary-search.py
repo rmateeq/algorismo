a1b2be058d95bc1736ceb6b5b8cd1e1e30b568df
Muhammad Ateeq <rmateeq@yahoo.com>
2024-11-09 02:02:08 +0500

    Create recursive-binary-search.py

---
+++ 03-Binary-Search/recursive-binary-search.py
@@ -0,0 +1,21 @@
+def recursive_binary_search(A, target, low, high):
+
+    if low > high:
+        return -1
+
+    mid = (low + high) // 2
+
+    if A[mid] == target:
+        return mid
+    elif A[mid] > target:
+        return recursive_binary_search(A, target, ____, ____)  
+    else:
+        return recursive_binary_search(A, target, ____, ____) 
+
+A = [3, 7, 12, 18, 23, 27, 30, 35] 
+target = 7
+result = recursive_binary_search(A, target, 0, len(A)-1) 
+if result != -1: 
+    print(f'Target found at index {result}') 
+else: 
+    print('Target not found')
